a32b81ebbc9ff0633909ccd28c9b858d74b6b9ab
Damir <Damir>
2023-08-31 16:21:39 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+class Main {
+    public static void main(final String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/test/java/EducationTests.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EducationTests {
+    @Test
+    public void firstTest() {
+        String str = "Hello World!";
+        Assertions.assertEquals(str, "Hello World!");
+    }
+}
