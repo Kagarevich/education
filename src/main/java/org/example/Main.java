edcf1f0180da22b8f3161ac20f396d287cc598ce
Damir <Damir>
2023-08-31 16:46:24 +0300

    add comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
 class Main {
+    /**
+     * @param args command-line arguments
+     */
     public static void main(final String[] args) {
+        printHelloWorld();
+    }
+    /**
+     * print to console "Hello, World!"
+     */
+    static void printHelloWorld() {
         System.out.println("Hello, World!");
     }
 }
